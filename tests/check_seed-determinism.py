7f299583ae14c077add0327438a518fd301d4ba8
greenKac <greenkac.greener@gmail.com>
2025-03-18 22:08:55 +0100

    feat: added a "tests" folder + seed determinism test

---
+++ tests/check_seed-determinism.py
@@ -0,0 +1,13 @@
+from fastn import noise
+from random import Random
+
+rand = Random(1)
+
+seeds = [rand.randint(0, i) * i * i for i in range(128)]
+samplePositions = [(x / 0.17865, y * 0.189652) for x in range(128) for y in range(128)]
+
+for seed in seeds:
+    np1 = noise.PerlinNoise(seed)
+    np2 = noise.PerlinNoise(seed)
+    for samplePos in samplePositions:
+        assert np1.Sample(*samplePos) == np2.Sample(*samplePos)
